b1af66f73419b6fae4258402fad5fa40bedbbd32
BlueBeetle97 <BlueBeetle97@users.noreply.github.com>
2018-10-20 08:00:39 +0300

    Progress on Server side

--- Server.java
+++ Server.java
@@ -1,5 +1,12 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.HashSet;
 
 /**
@@ -7,11 +14,10 @@ import java.util.HashSet;
  */
 public class Server {
     // The ServerSocket we'll use for accepting new connections
-    private ServerSocket ss;
+    private ServerSocket server;
 
-    //HashMap that contains users with their nicknames as keys;
+    // HashMap that contains users with their nicknames as keys;
     private HashMap<String, User> clients;
-
 
     // Constructor
     public Server() {
@@ -21,28 +27,31 @@ public class Server {
 
     private void listen(int port) throws IOException {
         // Create the ServerSocket
-        ss = new ServerSocket(port);
+        server = new ServerSocket(port);
 
         // Tell the world we're ready to go
-        System.out.println("Listening on " + ss);
+        System.out.println("Listening on " + server);
 
         // Keep accepting connections forever
         while (true) {
             // Grab the next incoming connection
-            Socket client = ss.accept();
+            Socket client = server.accept();
             // Tell the world we've got it
             System.out.println("Connection from " + client);
 
-            //set a nickname for the new user
+            // set a nickname for the new user
             String nickname;
             do {
-                nickname = ("user" + (int)(Math.random() * 1001));
+                nickname = ("user" + (int) (Math.random() * 1001));
             } while (clients.containsKey(nickname));
 
-            //create new user
+            // announce the nickname
+            System.out.println("nickname was set to " + nickname);
+
+            // create new user
             User newUser = new User(client, nickname);
 
-            //store new user
+            // store new user
             clients.put(nickname, newUser);
 
         }
@@ -54,12 +63,12 @@ public class Server {
     public static void main(String[] args) {
         try {
             // Get the port # from the command line
-            if(args.length != 1){
+            if (args.length != 1) {
                 System.out.println("Improper declaration!");
                 System.out.println("Proper usage: java Server <port>");
                 return;
             }
-            int port = Integer.parseInt(args[0]);  
+            int port = Integer.parseInt(args[0]);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Improper declaration!");
             System.out.println("Proper usage: java Server <port>");
@@ -80,12 +89,102 @@ public class Server {
     }
 }
 
-Class User {
+class UserHandler extends Thread {
+    // The Server that spawned us
+    private Server server;
+
+    // The user we are handing
+    private User user;
+
+    // Constructor
+    public UserHandler(Server server, User user) {
+        // Save the parameters
+        this.server = server;
+        this.user = user;
+
+        // announce online users
+        this.server.broadcastOnlineUsers();
+
+        // Start up the thread
+        start();
+    }
+
+    public void run() {
+        try {
+            // Create a DataInputStream for communication; the client
+            // is using a DataOutputStream to write to us
+            DataInputStream din = new DataInputStream(user.getInputStream());
+
+            // Over and over, forever ...
+            while (true) {
+                // ... read the next message ...
+                String message = din.readUTF();
+
+                // ... tell the world ...
+                System.out.println("Sending " + message);
+
+                // ... and have the server send it to all clients
+                server.broadcastToAll(message);
+            }
+        } catch (EOFException e) {
+            // No error message needed
+        } catch (IOException e) {
+            // Error message needed
+            e.printStackTrace();
+        } finally {
+            // The connection is closed for one reason or another,
+            // so have the server dealing with it
+            server.removeConnection(user);
+        }
+    }
+
+    // Clean contents of the message from offensive
+    private String cleanMessage(String message) {
+        try {
+            URL url = new URL(
+                    "https://raw.githubusercontent.com/BlueBeetle97/JavaChat/master/banned_words.txt?token=AWL8DgsfOvf4sn2MlIcKxVBBHIBYWrZxks5b0-UdwA%3D%3D");
+            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+            String str;
+            while ((str = in.readLine()) != null) {
+                message = message.replace(str, "<family friendly content>");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return message;
+    }
+}
+
+class User {
     private Socket client;
     private String nickname;
+    private OutputStream output;
+    private InputStream input;
 
     public User(Socket client, String nickname) {
         this.client = client;
+        this.nickname = nickname;
+        this.output = client.getOutputStream();
+        this.input = client.getInputStream();
+    }
+
+    public PrintStream getOutStream() {
+        return this.input;
+    }
+
+    public InputStream getInputStream() {
+        return this.output;
+    }
+
+    public String getNickname() {
+        return this.nickname;
+    }
+
+    public Socket getSocket() {
+        return this.client;
+    }
+
+    public void changeNickname(String nickname) {
         this.nickname = nickname;
     }
 }
